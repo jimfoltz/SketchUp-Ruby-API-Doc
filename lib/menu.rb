206d01f6522ef92e713249e220a79e9f61e1025d
Jim Foltz <jim.foltz@gmail.com>
2011-11-06 11:18:00 -0500

    Menu#add_item

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,7 +1,25 @@
 module Sketchup
     class Menu
-        def add_item
+	# add_item may take 2 forms.
+	#
+	# @overload add_item(menu_item)
+	#   @param [String] menu_item the name of the menu item, or a UI::Command object. 
+	#   @example
+	#     menu = UI.menu("Plugins")
+	#     menu_id = menu.add_item("Calculate Area"} { calc_area }
+	#
+	# @overload add_item(command)
+	#   @param [UI::Command] command a UI::Command object
+	#   @example
+	#     menu = UI.menu("Plugins")
+	#     cmd = UI::Command.new("Calculate Area") { calc_area }
+	#     menu_id = menu.add_item(cmd)
+	#
+	# @return [menu_id] a unique numeric id number
+
+        def add_item(item)
         end
+
         def add_separator
         end
         def add_submenu
