997a5889ce2edba4035f63c166c1ed091c75e60f
Jim Foltz <jim.foltz@gmail.com>
2011-11-07 04:29:36 -0500

    ConstructionPoint

--- lib/constructionpoint.rb
+++ lib/constructionpoint.rb
@@ -1,12 +1,19 @@
 module Sketchup
-class ConstructionPoint < Drawingelement
-    # @private
-    def initialize(pt = [0, 0, 0])
-	@position = Geom::Point3d.new(pt)
-    end
+    class ConstructionPoint < Drawingelement
 
-    def position
-	@position
+        # @private
+        def initialize(pt = [0, 0, 0])
+            @position = Geom::Point3d.new(pt)
+        end
+
+        # @example
+        #   pt = Sketchup.active_model.entities.add_cpoint([1, 1, 0])
+        #   pos = pt.position
+        #
+        # @return [Point3d]
+        #
+        def position
+            @position
+        end
     end
-end
 end
