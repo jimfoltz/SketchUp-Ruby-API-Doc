6152f12779e88912ec93bc41e01dcf0d968c3f4b
Jim Foltz <jim.foltz@gmail.com>
2013-01-13 09:14:07 -0500

    cleanup and whitespace for importer.rb

--- lib/importer.rb
+++ lib/importer.rb
@@ -1,21 +1,29 @@
 module Sketchup
-class Importer
-    ImportCanceled = 2
-    ImportFail = 1
+  class Importer
+
+    ImportSuccess      = 0
+    ImportFail         = 1
+    ImportCanceled     = 2
+    ImporterNotFound   = 3
     ImportFileNotFound = 4
-    ImportSuccess = 0
-    ImporterNotFound = 3
+
     def description
     end
+
     def do_options
     end
+
     def file_extension
     end
+
     def id
     end
+
     def load_file
     end
+
     def supports_options?
     end
-end
+
+  end
 end
