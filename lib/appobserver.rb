ad51d7da0f16ad2f6c1f806df9472583fc9e382e
Jim Foltz <jim.foltz@gmail.com>
2014-04-06 11:16:24 -0400

    appobserver

--- lib/appobserver.rb
+++ lib/appobserver.rb
@@ -1,12 +1,38 @@
 module Sketchup
-class AppObserver
+  # {http://www.sketchup.com/intl/en/developer/docs/ourdoc/appobserver AppObserver Docs}
+  class AppObserver
+
+    # @since SketchUp 2014
+    # @example
+    #    def expectsStartupModelNotifications
+    #        return true
+    #    end
+    #     
+    # Return `true` to receive onNewModel and onOpenModel calls for
+    # startup models. Return false or simply not implement the method in
+    # order to not receive these calls (which was the behavior prior to
+    # SketchUp 2014).
+    def expectsStartupModelNotifications
+    end
+
     def onNewModel
+      return nil
     end
+
+    # @return nil
     def onOpenModel
+      return nil
     end
+
+    # @return nil
     def onQuit
+      return nil
     end
+
+    # @return nil
     def onUnloadExtension
+      return nil
     end
-end
+
+  end
 end # module Sketchup
