d83ad26771bf275734fdb45421343bb318f88206
Jim Foltz <jim.foltz@gmail.com>
2014-04-06 11:15:56 -0400

    Enumerable

--- lib/enumerable.rb
+++ lib/enumerable.rb
@@ -6,40 +6,163 @@ module Enumerable
     end
     def any?
     end
+
+    # @since SketchUp 2014
+    def chunk
+    end
+
     def collect
     end
+
+    # @since SketchUp 2014
+    def collect_concat
+    end
+    
+    # @since SketchUp 2014
+    def count
+    end
+    
+    # @since SketchUp 2014
+    def cycle
+    end
+
     def detect
     end
+    
+    # @since SketchUp 2014
+    def drop
+    end
+    
+    # @since SketchUp 2014
+    def drop_while
+    end
+    
+    # @since SketchUp 2014
+    def each_cons
+    end
+    
+    # @since SketchUp 2014
+    def each_entry
+    end
+    
+    # @since SketchUp 2014
+    def each_slice
+    end
+
     def each_with_index
     end
+    
+    # @since SketchUp 2014
+    def each_with_object
+    end
+
     def entries
     end
+
     def find
     end
+
     def find_all
     end
+    
+    # @since SketchUp 2014
+    def find_index
+    end
+    
+    # @since SketchUp 2014
+    def first
+    end
+    
+    # @since SketchUp 2014
+    def flat_map
+    end
+
     def grep
     end
+    
+    # @since SketchUp 2014
+    def group_by
+    end
+
     def inject
     end
+    
+    # @since SketchUp 2014
+    def lazy
+    end
+
     def map
     end
     def max
     end
+    
+    # @since SketchUp 2014
+    def max_by
+    end
+
     def member?
     end
     def min
     end
+    
+    # @since SketchUp 2014
+    def min_by
+    end
+    
+    # @since SketchUp 2014
+    def minmax
+    end
+    
+    # @since SketchUp 2014
+    def minmax_by
+    end
+    
+    # @since SketchUp 2014
+    def none?
+    end
+    
+    # @since SketchUp 2014
+    def one?
+    end
+
     def partition
     end
+    
+    # @since SketchUp 2014
+    def reduce
+    end
+    
     def reject
     end
+
+    # @since SketchUp 2014
+    def reverse_each
+    end
+
     def select
     end
+
+    # @since SketchUp 2014
+    def slice_before
+    end
+
     def sort
     end
     def sort_by
     end
+
+    # @since SketchUp 2014
+    def take
+    end
+
+    # @since SketchUp 2014
+    def take_while
+    end
+
+    # @since SketchUp 2014
+    def to_a
+    end
+
     def zip
     end
 end
