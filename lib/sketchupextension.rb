e4309bd4394620c6ddc8f73469e2b30fb3a2ad99
Thomas Thomassen <thomas@thomthom.net>
2014-12-18 18:07:42 +0100

    Adding getters and setters for SketchupExtension.

--- lib/sketchupextension.rb
+++ lib/sketchupextension.rb
@@ -1,37 +1,62 @@
 class SketchupExtension
+
+    # @since SketchUp 6
     def copyright
     end
-    def copyright=
+
+    # @since SketchUp 6
+    def copyright=(string)
     end
+
+    # @since SketchUp 6
     def creator
     end
-    def creator=
+
+    # @since SketchUp 6
+    def creator=(string)
     end
+
+    # @since SketchUp 6
     def description
     end
-    def description=
+
+    # @since SketchUp 6
+    def description=(string)
     end
+
 	# @since SketchUp 8.0 M2
 	def load_on_start?
 	end
+
 	# @since SketchUp 8.0 M2
 	def loaded?
 	end
+
 	# @since SketchUp 8.0 M2
 	def registered?
 	end
+
 	# @since SketchUp 8.0 M2
 	def uncheck
 	end
-	
+
+	# @since SketchUp 6
     def name
     end
-    def name=
+
+    # @since SketchUp 6
+    def name=(string)
     end
+
+    # @since SketchUp 6
     def new
     end
+
+    # @since SketchUp 6
     def version
     end
-    def version=
+
+    # @since SketchUp 6
+    def version=(string)
     end
 end
