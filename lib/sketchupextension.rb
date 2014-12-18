8adbe8ec11c025ddbe20c1b21846aee2fe89d344
Jim Foltz <jim.foltz@gmail.com>
2014-12-18 16:12:20 -0500

    indent

--- lib/sketchupextension.rb
+++ lib/sketchupextension.rb
@@ -1,62 +1,62 @@
 class SketchupExtension
 
-    # @since SketchUp 6
-    def copyright
-    end
+  # @since SketchUp 6
+  def copyright
+  end
 
-    # @since SketchUp 6
-    def copyright=(string)
-    end
+  # @since SketchUp 6
+  def copyright=(string)
+  end
 
-    # @since SketchUp 6
-    def creator
-    end
+  # @since SketchUp 6
+  def creator
+  end
 
-    # @since SketchUp 6
-    def creator=(string)
-    end
+  # @since SketchUp 6
+  def creator=(string)
+  end
 
-    # @since SketchUp 6
-    def description
-    end
+  # @since SketchUp 6
+  def description
+  end
 
-    # @since SketchUp 6
-    def description=(string)
-    end
+  # @since SketchUp 6
+  def description=(string)
+  end
 
-	# @since SketchUp 8.0 M2
-	def load_on_start?
-	end
+  # @since SketchUp 8.0 M2
+  def load_on_start?
+  end
 
-	# @since SketchUp 8.0 M2
-	def loaded?
-	end
+  # @since SketchUp 8.0 M2
+  def loaded?
+  end
 
-	# @since SketchUp 8.0 M2
-	def registered?
-	end
+  # @since SketchUp 8.0 M2
+  def registered?
+  end
 
-	# @since SketchUp 8.0 M2
-	def uncheck
-	end
+  # @since SketchUp 8.0 M2
+  def uncheck
+  end
 
-	# @since SketchUp 6
-    def name
-    end
+  # @since SketchUp 6
+  def name
+  end
 
-    # @since SketchUp 6
-    def name=(string)
-    end
+  # @since SketchUp 6
+  def name=(string)
+  end
 
-    # @since SketchUp 6
-    def new
-    end
+  # @since SketchUp 6
+  def new
+  end
 
-    # @since SketchUp 6
-    def version
-    end
+  # @since SketchUp 6
+  def version
+  end
 
-    # @since SketchUp 6
-    def version=(string)
-    end
+  # @since SketchUp 6
+  def version=(string)
+  end
 end
