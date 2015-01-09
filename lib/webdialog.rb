4629c7330c583d232691ddc26fe516e57649588f
Thomas Thomassen <thomas@thomthom.net>
2015-01-09 13:15:23 +0100

    Flesh out UI::WebDialog's params.

--- lib/webdialog.rb
+++ lib/webdialog.rb
@@ -1,70 +1,204 @@
 module UI
-class WebDialog
-    def add_action_callback
+  class WebDialog
+
+    # @param [String] callback_name
+    #
+    # @yield [dialog, param]
+    # @yieldparam [UI::WebDialog] dialog
+    # @yieldparam [String] param
+    #
+    # @return [Nil]
+    def add_action_callback(callback_name)
     end
-    def allow_actions_from_host
+
+    # @param [String] hostname
+    #
+    # @return [Nil]
+    def allow_actions_from_host(hostname)
     end
+
+    # @return [Nil]
     def bring_to_front
     end
+
+    # @return [Nil]
     def close
     end
-    def execute_script
+
+    # @param [String] script
+    #
+    # @return [Nil]
+    def execute_script(script)
     end
+
+    # @return [String]
     def get_default_dialog_color
     end
-    def get_element_value
+
+    # @param [String] element_id
+    #
+    # @return [Nil]
+    def get_element_value(element_id)
     end
+
+    # @return [Integer]
     def max_height
     end
-    def max_height=
+
+    # @param [Integer] value
+    #
+    # @return [Integer]
+    def max_height=(value)
     end
+
+    # @return [Integer]
     def max_width
     end
-    def max_width=
+
+    # @param [Integer] value
+    #
+    # @return [Integer]
+    def max_width=(value)
     end
+
+    # @return [Integer]
     def min_height
     end
-    def min_height=
+
+    # @param [Integer] value
+    #
+    # @return [Integer]
+    def min_height=(value)
     end
+
+    # @return [Integer]
     def min_width
     end
-    def min_width=
+
+    # @param [Integer] value
+    #
+    # @return [Integer]
+    def min_width=(value)
     end
-    def navigation_buttons_enabled=
+
+    # @param [Boolean] value
+    #
+    # @return [Boolean]
+    def navigation_buttons_enabled=(value)
     end
+
+    # @return [Boolean]
     def navigation_buttons_enabled?
     end
-    def new
+
+    # @overload new(title, scrollable, preferences_key, width, height, left, top, resizable)
+    #   @param [String] title
+    #   @param [Boolean] scrollable
+    #   @param [String] preferences_key
+    #   @param [Integer] width
+    #   @param [Integer] height
+    #   @param [Integer] left
+    #   @param [Integer] top
+    #   @param [Boolean] resizable
+    #
+    # @overload new(options)
+    #   @param preferences_key [Hash] options
+    #   @option options [String] :dialog_title
+    #   @option options [String] :preferences_key
+    #   @option options [Boolean] :scrollable
+    #   @option options [Boolean] :resizable
+    #   @option options [Integer] :top
+    #   @option options [Integer] :left
+    #   @option options [Integer] :width
+    #   @option options [Integer] :height
+    #   @option options [Boolean] :mac_only_use_nswindow
+    #
+    # @return [UI::WebDialog]
+    def new(*args)
     end
+
+    # @param [String] url
+    # @param [String] data
+    #
+    # @return [Nil]
     def post_url
     end
 
     # @since SketchUp 2014
-    # @return [Integer] screen scale factor
+    # @return [Float] screen scale factor
     def screen_scale_factor
     end
 
-    def set_background_color
+    # @param [String] color
+    #
+    # @return [Nil]
+    def set_background_color(color)
     end
-    def set_file
+
+    # @param [String] filename
+    # @param [String] path
+    #
+    # @return [Nil]
+    def set_file(filename, path = nil)
     end
-    def set_full_security=
+
+    # @since SketchUp 7.1
+    #
+    # @param [Boolean] value
+    #
+    # @return [UI::WebDialog]
+    def set_full_security(value)
     end
-    def set_html
+
+    # @param [String] html
+    #
+    # @return [Nil]
+    def set_html(html)
     end
+
+    # @yield [dialog]
+    # @yieldparam [UI::WebDialog] dialog
+    #
+    # @return [Nil]
     def set_on_close
     end
-    def set_position
+
+    # @param [Integer] left
+    # @param [Integer] top
+    #
+    # @return [Nil]
+    def set_position(left, top)
     end
-    def set_size
+
+    # @param [Integer] width
+    # @param [Integer] height
+    #
+    # @return [Nil]
+    def set_size(width, height)
     end
-    def set_url
+
+    # @param [String] url
+    #
+    # @return [Nil]
+    def set_url(url)
     end
+
+    # @yield [dialog]
+    # @yieldparam [UI::WebDialog] dialog
+    #
+    # @return [Nil]
     def show
     end
+
+    # @yield [dialog]
+    # @yieldparam [UI::WebDialog] dialog
+    #
+    # @return [Nil]
     def show_modal
     end
+
     def visible?
     end
-end
+
+  end
 end
