0c78a99771f660212ccea125cdf1fb1940437433
Jim Foltz <jim.foltz@gmail.com>
2015-02-09 07:56:53 -0500

    included Enumerable in ExtensionsManager

--- lib/extensionsmanager.rb
+++ lib/extensionsmanager.rb
@@ -1,28 +1,32 @@
 module Sketchup
 
-# @since SketchUp 8.0 M2
-class ExtensionsManager
-	# @return [SketchupExtension]
-	# @overload [] Integer
-	#   @param [Integer] n
-	# @overload [](String)	
-	def []
-	end
-	
-	def each
-	end
-	
-	# The keys method is used to get a list of keys in the ExtensionsManager, which are the same as the names of the extensions.
-	# @return [Array<String>]  an Array of Strings
-	def keys
-	end
-	
-	# @return [Number]
-	def size
-	end
-	private
-	def intitialize
-	end
-	
-end
+  # @since SketchUp 8.0 M2
+  class ExtensionsManager
+
+    include Enumerable
+
+    # @return [SketchupExtension]
+    # @overload [] Integer
+    #   @param [Integer] n
+    # @overload [](String)	
+    def []
+    end
+
+    def each
+    end
+
+    # The keys method is used to get a list of keys in the ExtensionsManager, which are the same as the names of the extensions.
+    # @return [Array<String>]  an Array of Strings
+    def keys
+    end
+
+    # @return [Number]
+    def size
+    end
+
+    private
+    def intitialize
+    end
+
+  end
 end
