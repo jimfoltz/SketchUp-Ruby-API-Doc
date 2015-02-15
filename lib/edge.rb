d4142af79440a96950e99f66de1253d7f20c2062
Thomas Thomassen <thomas@thomthom.net>
2015-02-14 20:07:15 -0700

    Add arguments to SketchUp::Edge class.

--- lib/edge.rb
+++ lib/edge.rb
@@ -1,55 +1,105 @@
 module Sketchup
-    class Edge < Drawingelement
+  class Edge < Drawingelement
 
-	# @private
-	def initialize(pt1, pt2)
-	end
-
-        # @example
-        #   model = Sketchup.active_model
-        #   edge = model.selection[0]
-        #   ents = edge.all_connected
-        #   ==> [<Sketchup::Edge:0x787a440>, #<Sketchup::Edge:0x787a42c>, #<Sketchup::Face:0x787a418>, #<Sketchup::Edge:0x787a468>, #<Sketchup::Edge:0x787a404>]
-        #
-        # @return [Array] an array of the edge and its connected entities.
-        def all_connected
-        end
-
-        def common_face
-        end
-        def curve
-        end
-        def end
+    # @private
+    def initialize(pt1, pt2)
     end
+
+    # @example
+    #   model = Sketchup.active_model
+    #   edge = model.selection[0]
+    #   ents = edge.all_connected
+    #   ==> [<Sketchup::Edge:0x787a440>, #<Sketchup::Edge:0x787a42c>, #<Sketchup::Face:0x787a418>, #<Sketchup::Edge:0x787a468>, #<Sketchup::Edge:0x787a404>]
+    #
+    # @return [Array] an array of the edge and its connected entities.
+    def all_connected
+    end
+
+    # @param [Sketchup::Edge] edge
+    #
+    # @return [Sketchup::Face, Nil]
+    def common_face(edge)
+    end
+
+    # @return [Sketchup::Curve, Sketchup::ArcCurve, Nil]
+    def curve
+    end
+
+    # @return [Sketchup::Vertex]
+    def end
+    end
+
+    # @return [Sketchup::Edge]
     def explode_curve
     end
+
+    # @return [Array<Sketchup::Face>]
     def faces
     end
+
+    # @return [Integer]
     def find_faces
     end
+
+    # @return [Length]
     def length
     end
+
+    # @return [Array(Geom::Point3d, Geom::Vector3d)]
     def line
     end
-    def other_vertex
+
+    # @param [Sketchup::Edge] edge
+    #
+    # @return [Sketchup::Vertex]
+    def other_vertex(edge)
     end
-    def reversed_in?
+
+    # @param [Sketchup::Face] face
+    #
+    # @return [Boolean]
+    def reversed_in?(face)
     end
-    def smooth=
+
+    # @param [Boolean] value
+    #
+    # @return [Boolean]
+    def smooth=(value)
     end
+
+    # @return [Boolean]
     def smooth?
     end
+
+    # @param [Boolean] value
+    #
+    # @return [Boolean]
     def soft=
     end
+
+    # @return [Boolean]
     def soft?
     end
+
+    # @param [Geom::Point3d] point
+    #
+    # @param [Sketchup::Edge, Nil]
     def split
     end
+
+    # @return [Sketchup::Vertex]
     def start
     end
-    def used_by?
+
+    # @param [Sketchup::Edge, Sketchup::Vertex] edge_or_vertex
+    #
+    # @return [Boolean]
+    def used_by?(edge_or_vertex)
     end
+
+    # @return [Array<Sketchup::Vertex>]
     def vertices
     end
-end
+
+  end
 end
