4d9ddb54689005b2e31120bb3274172de0d9ef10
Jim Foltz <jim.foltz@gmail.com>
2016-03-06 06:08:34 -0500

    Added rough Axes class

---
+++ lib/axes.rb
@@ -0,0 +1,44 @@
+module Sketchup
+
+   # @since SketchUp 2016
+   class Axes
+
+      # @return Array<Vector3d>
+      def axes
+      end
+
+      # @return [Point3d]
+      def origin
+      end
+
+      # @return [Axes]
+      def set(point, xaxis, yaxis, zaxis)
+      end
+
+      # @return [Array]
+      def sketch_plane
+      end
+
+      # @return [Array] a point and 3 vectors
+      def to_a
+      end
+
+      # @return [Transformation]
+      def transformation
+      end
+
+      # @return [Vector3d]
+      def xaxis
+      end
+
+      # @return [Vector3d]
+      def yaxis
+      end
+
+      # @return [Vector3d]
+      def zaxis
+      end
+
+   end
+end
+
